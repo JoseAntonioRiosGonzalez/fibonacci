08a1446dc49475172f70d8aadca2b4a63ac9421e
José Antonio Ríos González <116028548+JoseAntonioRiosGonzalez@users.noreply.github.com>
2022-10-26 01:35:41 +0200

    Add files via upload

---
+++ Fibonnaci.cs
@@ -0,0 +1,31 @@
+﻿//Fibonacci series in c#. The user will tell us the number he wants for the series
+
+using System;
+
+namespace fibonacci
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int totalNumber, K;
+            int A, B, C;
+
+            Console.Write("Tell me the numbers you want in the Fibonacci series: ");
+            totalNumber = int.Parse(Console.ReadLine());
+
+            A = 1; B = 1;
+            Console.Write(A + " " + B + " ");
+            for (K = 3; K <= totalNumber; K++)
+            {
+                C = A + B;
+                Console.Write(C + " ");
+
+                //we change the values
+                A = B;
+                B = C;
+            }
+            Console.WriteLine();
+        }
+    }
+}
